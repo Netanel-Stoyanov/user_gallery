21fe8d202e2ae2bda8cb2249a11754207fd74b26
Netanel-Stoyanov <83516612+Netanel-Stoyanov@users.noreply.github.com>
2021-10-23 21:27:14 +0300

    adding redux and actions

---
+++ src/redux/actions/index.js
@@ -0,0 +1,80 @@
+import {authApi} from "../../service/AuthenticatedApiService";
+import {GalleryItemModule} from "../../module/GalleryItemModule";
+import {Subject} from "rxjs";
+
+const dataWasChange = new Subject();
+const redirect = new Subject();
+
+export const addImage = (image) => {
+    return {
+        type: "IMAGE/ADD",
+        payload: image,
+    };
+};
+
+export const editImage = (imageId, image) => {
+    return {
+        type: "IMAGE/EDIT",
+        payload: {imageId, image},
+    };
+};
+
+export const deleteImage = (imageId) => {
+    return {
+        type: "IMAGE/DELETE",
+        payload: imageId,
+    };
+};
+
+export const getAllImage = (images) => {
+    return {
+        type: "IMAGE/DELETE",
+        payload: images,
+    };
+};
+
+export const getData = () => (dispatch, getState) => {
+        authApi.get("http://localhost:8089/gallery")
+            .then((response) => {
+               const galleryItems = response.data.map((item) => new GalleryItemModule(item._id,
+                    item.title, item.url, item.description));
+                dispatch(getAllImage(galleryItems));
+            }).catch((err) => {
+            console.error(err);
+            redirect.next("LOGIN")
+        })
+}
+
+export const addImageToDb = (img) => (dispatch) => {
+    authApi.post("http://localhost:8089/gallery", img)
+        .then((response) => {
+            const newImage = response.data;
+            dispatch(addImage(newImage))
+        }).catch((err) => {
+        console.error(err);
+        redirect.next("LOGIN");
+    })
+}
+
+const deleteImageFromDb = (id) => (dispatch) => {
+    authApi.delete("http://localhost:8089/gallery/" + id)
+        .then((response) => {
+            dispatch(deleteImage(id));
+        }).catch((err) => {
+        console.error(err);
+    })
+}
+
+export const editImageInDb = (id, img) => (dispatch) => {
+    authApi.put("http://localhost:8089/gallery/" + id, img)
+        .then((response) => {
+            dispatch(editImage(id, img))
+            console.log(response)
+        }).catch((err) => {
+        console.error(err);
+    })
+}
+
+export const getRedirect = () => {
+    return redirect.asObservable();
+}
