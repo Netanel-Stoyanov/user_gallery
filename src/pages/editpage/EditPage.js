26423d262dc831fac0947ebc222ac59ef68c9ec7
Netanel-Stoyanov <83516612+Netanel-Stoyanov@users.noreply.github.com>
2021-10-24 23:18:54 +0300

    new problem i cant edit the input value

--- src/pages/editpage/EditPage.js
+++ src/pages/editpage/EditPage.js
@@ -2,15 +2,13 @@ import {useHistory} from "react-router-dom";
 import {editImageInDb, setDescription, setTitle, setUrl} from "../../redux/actions";
 import {connect} from "react-redux";
 
-
 function EditPage(props) {
 
     const history = useHistory();
 
-    //this is in the first time render is undefined and i dont know why
-    //so i can't set props.setTitle(oneImage.title)
-    //look for the next comments down below
+
     const oneImage = props.galleryImage.find(image => image.id === props.match.params.id);
+
 
     console.log(oneImage)
     const onSubmit = () => {
@@ -29,39 +27,36 @@ function EditPage(props) {
         }
     }
 
+    const onSubmitValue = (event, valueType) => {
+        if (valueType === "title") {
+            props.setTitle(event.target.value);
+        } else if (valueType === "description"){
+            props.setDescription(event.target.value);
+        } else if (valueType === "url"){
+            props.setUrl(event.target.value);
+        }
+    }
+
     return (
         <form onSubmit={onSubmit}>
             <div className="form-group">
                 <label>Title</label>
-                <input type="text" onChange={(event => {
-                    props.setTitle(event.target.value)
-                })} className="form-control"
-                       //in the first time when you click edit value should
-                    //be as the image that im got from the array
-                    //and if im set the props up in the useEfect
-                    //when im trying to set some value in the input
-                    //its rerender the component because the on change
-                    //on the input setTitle and then its always re put the image value
-                    //that im got from the reducer state
-                    //please look how the action and the reducer works
-                    //i can't understand why the first time the image is undefined
-                    //so i can't use useEfect to set the title only in the first time
-                    //Also about the rest inputs in the component
-                value={props.title}/>
+                {/* can't edit the input value  */}
+                <input type="text" onSubmit={(event => {onSubmitValue(event)})} className="form-control"
+                       value={oneImage? oneImage.title : ""}/>
 
             </div>
             <div className="form-group">
                 <label>Description</label>
-                <input type="text" onChange={(event => {props.setDescription(event.target.value)
-                })}   className="form-control"
-                value={props.description}/>
+                {/* can't edit the input value  */}
+                <input type="text" onSubmit={(event => {onSubmitValue(event)})}   className="form-control"
+                value={oneImage? oneImage.description : ""}/>
             </div>
             <div className="form-group">
                 <label>URL</label>
-                <input type="text" onChange={(event => {
-                    props.setUrl(event.target.value)
-                })} className="form-control"
-                       value={props.url} />
+                {/* can't edit the input value  */}
+                <input type="text" onSubmit={(event => {onSubmitValue(event)})} className="form-control"
+                       value={oneImage? oneImage.url : ""} />
             </div>
             <button type="submit" className="btn btn-primary mt-3">Edit Image</button>
         </form>
